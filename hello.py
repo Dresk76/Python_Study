7cf4daaf343ae93869c61b69faff671cd6e533c6
Felipe Agudelo <felipeagudeloolaya@gmail.com>
2026-01-28 13:32:13 -0500

    Actualización modulo Hola mundo

--- hello.py
+++ hello.py
@@ -1,2 +1,77 @@
-# Esto es un comentario
+'''
+1. Imprime "¡Hola Mundo!" por consola.
+''' 
 print('Hello World!')
+print('--------------------------------------------')
+
+'''
+2. Escribe un comentario de una sola línea explicando qué 
+hace el código del Ejercicio 1.
+'''
+# El código del ejericio 1 muestra por consola la frase "Hello World!" con la funcion "print"
+
+'''
+3. Imprime tu nombre y edad en la misma línea utilizando la 
+función print.
+'''
+print('- Nombre: Felipe Agudelo' + '\n' + '- Edad: 30')
+print('--------------------------------------------')
+
+'''
+4. Usa la función type() para imprimir el tipo de dato de 
+una cadena de texto, un número entero y un número 
+decimal.
+'''
+print(type("String"))
+print(type(1))
+print(type(2.5))
+print('--------------------------------------------')
+
+'''
+5. Escribe un comentario en varias líneas explicando qué 
+son los tipos de datos en Python.
+'''
+'''
+Los tipos de datos en Python sirven para guardar información
+como strings, int y float.
+'''
+
+'''
+6. Imprime el resultado de concatenar dos cadenas de texto, 
+por ejemplo: "Hola" y "Mundo".
+'''
+print('Hola ' + 'Mundo')
+print('--------------------------------------------')
+
+'''
+7. Crea una variable para almacenar tu nombre, otra para tu 
+edad, e imprime ambas en la misma línea.
+'''
+name = 'Felipe'
+age = 30;
+print('Mi nombre es: ' + name + ' y tengo ' + str(age), ' años.')
+print('--------------------------------------------')
+
+'''
+8. Escribe un programa que solicite al usuario su nombre y 
+lo imprima junto con un saludo.
+'''
+userName = input('Ingrese su nombre: ')
+print('Hola', userName + ', Bienvenid@ al curso de Python.')
+print('--------------------------------------------')
+
+'''
+9. Usa print() para mostrar el resultado de la suma de dos 
+números enteros y el tipo de dato resultante.
+'''
+a = 7 # Esta variable guarda el primer numero de la operación
+b = 3 # Esta variable guarda el segundo numero de la operación
+result = a + b # # Esta variable guarda el resultado de la operación
+print('El resultado es: ' + str(result))
+print('--------------------------------------------')
+
+'''
+10. Comenta el código del Ejercicio 9, y explica qué 
+hace cada línea usando comentarios de una sola línea.
+'''
+
